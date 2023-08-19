70a1542803f1b50c22c5feee5ff91357432db9b5
Chirag Bellani <112893293+bendict-github@users.noreply.github.com>
2023-08-19 20:12:21 +0530

    NewsMonkey_reactapp(using funtional based component)

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className="my-3">
-                <div className="card">
-                    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
-                        <span className=" badge rounded-pill bg-warning" style={{ color: 'black' }}> {source}
-                        </span>
-                    </div>
-                    <img src={!imageUrl ? "https://startup.mp.gov.in/assets/img/img-not-found.png" : imageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}  </h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text" style={{ backgroundColor: 'pink' }}>By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm " style={{ backgroundColor: '#539f53', color: 'black', padding: '4px 6px' }}> Read More</a>
-                    </div>
+const NewsItem = (props) => {
+
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className="my-3">
+            <div className="card">
+                <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+                    <span className=" badge rounded-pill bg-warning" style={{ color: 'black' }}> {source}
+                    </span>
                 </div>
-            </div >
-        )
-    }
+                <img src={!imageUrl ? "https://startup.mp.gov.in/assets/img/img-not-found.png" : imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}  </h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text" style={{ backgroundColor: 'pink' }}>By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm " style={{ backgroundColor: '#539f53', color: 'black', padding: '4px 6px' }}> Read More</a>
+                </div>
+            </div>
+        </div >
+    )
+
 }
 
 export default NewsItem
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import loading from './loading.gif'
+import React from 'react'
+import loading from './loading.gif';
 
-export class Spinner extends Component {
-    render() {
-        return (
-            <div className="text-center">
-                <img className="my-3" src={loading} alt="loading" height='180px' width='200px' />
-            </div>
-        )
-    }
+const Spinner = () => {
+
+    return (
+        <div className="text-center">
+            <img className="my-3" src={loading} alt="loading" height='180px' width='200px' />
+        </div>
+    )
+
 }
 
 export default Spinner
